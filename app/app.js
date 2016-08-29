c5e908c12c2f57c26c7a8b514f64db36f4b9cdcf
Federico Cano <canofedericomartin@gmail.com>
2016-08-29 01:59:08 -0300

    Layout con angular

---
+++ app/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('booking-app', ['ngCookies', 'ngResource', 'ngSanitize', 'ui.router', 'ui.bootstrap'])
+.config(function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+  $urlRouterProvider.otherwise('/');
+  return $locationProvider.html5Mode(true);
+})
+.config(function($stateProvider) {
+  return $stateProvider
+  .state('main', {
+    abstract: true,
+    views: {
+      'layout': {
+        templateUrl: "app/layout/layout.html"
+      },
+      'container@main': {
+        template: "<ui-view>"
+      },
+      'topbar@main': {
+        templateUrl: "app/layout/topbar.html"
+      },
+      'sidebar@main': {
+        templateUrl: "app/layout/sidebar.html"
+      }
+    }
+  });
+});
