6c3342d90c16bc14a805b3e2176adc9d0063dd6a
Ornella <om.bordino@gmail.com>
2016-08-31 01:05:17 -0300

    Se le quita 'Ver detalle' a la directiva cajita

--- app/dashboard/controllers/dashboard.js
+++ app/dashboard/controllers/dashboard.js
@@ -12,19 +12,18 @@
 
     self.Detalle = Detalle;
 
-    self.detalleVentas = new Detalle(3, 'Nuevas ventas!', '');
+    self.detalleVentas = new Detalle(3, 'Nuevas ventas!');
 
-    self.detalleMensajes = new Detalle(10, 'Nuevas mensajes!', '');
+    self.detalleMensajes = new Detalle(10, 'Nuevas mensajes!');
 
-    self.detalleQuejas = new Detalle(3, 'Nuevas quejas!', '');
+    self.detalleQuejas = new Detalle(3, 'Nuevas quejas!');
 
-    self.detalleCampanias = new Detalle(5, 'Nuevas campañas!', '');
+    self.detalleCampanias = new Detalle(5, 'Nuevas campañas!');
 
-    function Detalle(_cantidad, _mensaje, _accion) {
+    function Detalle(_cantidad, _mensaje) {
       return {
         cantidad: _cantidad,
-        mensaje: _mensaje,
-        accion: _accion
+        mensaje: _mensaje
       };
     }
   }
